56261417fcd303b0b503e083592ae2e35cc47079
TahiaBushra <bushratahia@gmail.com>
2023-12-17 14:05:05 +0600

    Model Done

---
+++ app.js
@@ -0,0 +1,35 @@
+"use strict"
+// const btn = document.querySelector(".btn");
+// btn.addEventListener("mouseover", () => {
+//     console.log("Button Hover");
+// });
+// btn.addEventListener("mouseout", () => {
+//     console.log("Button Unhover");
+// });
+// btn.addEventListener("mouseup", () => {
+//     console.log("Clicked");
+// });
+// document.addEventListener("keypress", (event) => {
+//     if (event.key === "Enter") {
+//         console.log("Enter is pressed");
+//     }
+// });
+
+// const btnOpenModel = document.querySelector(".btn_open_model");
+// const btnCloseModel = document.querySelector(".btn_close_model");
+// const model = document.querySelector(".model");
+
+// const openModel = () => {
+//     model.classList.remove("hidden");
+// };
+// const closeModel = () => {
+//     model.classList.add("hidden");
+// };
+
+// btnOpenModel.addEventListener("click", openModel);
+// btnCloseModel.addEventListener("click", closeModel);
+// document.addEventListener("keydown", (event) => {
+//     if (event.key === "Escape") {
+//         closeModel();
+//     }
+// });
